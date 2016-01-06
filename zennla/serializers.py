0e4b85ba98f8a2da09aaf42441434153504e590f
adesor <abhinav@grofers.com>
2016-01-06 18:50:10 +0530

    Rearrange serializers.py

--- zennla/serializers.py
+++ zennla/serializers.py
@@ -17,39 +17,7 @@ class ModelSerializer(object):
         instance.put()
         return instance
 
-    def create(self, data, model=None):
-        return self._save(data=data, instance=self.get_obj(model=model))
-
-    def to_dict_repr(self, obj):
-        dct = obj.to_dict(
-            include=self.include_fields,
-            exclude=self.exclude_fields
-        )
-        dct['id'] = obj.key.id()
-        for key, value in self.translate_fields.iteritems():
-            dct[value] = dct.pop(key)
-        for key in dct:
-            dct[key] = getattr(self, 'get_' + key, lambda _: dct[key])(obj)
-        return dct
-
-    def serialize(self, serializable):
-        if isinstance(serializable, ndb.Model):
-            return json.dumps(self.to_dict_repr(serializable))
-        elif isinstance(serializable, ndb.Query):
-            serializable = serializable.fetch()
-            return json.dumps([
-                self.to_dict_repr(obj) for obj in serializable
-            ])
-        raise NonSerializableException(
-            "Object of type {type} is not serializable".format(
-                type=type(serializable).__name__
-            )
-        )
-
-    def update(self, data, id, model=None, partial=False):
-        return self._save(data, instance=self.get_obj(id=id, model=model))
-
-    def validate(self, data, model=None):
+    def _validate(self, data, model=None):
         validated_data = {}
         model = model or self.model
         properties = model._properties
@@ -76,6 +44,9 @@ class ModelSerializer(object):
                     raise ValidationError(str(e))
         return validated_data
 
+    def create(self, data, model=None):
+        return self._save(data=data, instance=self.get_obj(model=model))
+
     def get_obj(self, id=None, model=None):
         model = model or self.model
         if not isinstance(model, ndb.model.MetaModel):
@@ -94,3 +65,32 @@ class ModelSerializer(object):
                 "Object with id {id} not found".format(id=id)
             )
         return obj
+
+    def serialize(self, serializable):
+        if isinstance(serializable, ndb.Model):
+            return json.dumps(self.to_dict_repr(serializable))
+        elif isinstance(serializable, ndb.Query):
+            serializable = serializable.fetch()
+            return json.dumps([
+                self.to_dict_repr(obj) for obj in serializable
+            ])
+        raise NonSerializableException(
+            "Object of type {type} is not serializable".format(
+                type=type(serializable).__name__
+            )
+        )
+
+    def to_dict_repr(self, obj):
+        dct = obj.to_dict(
+            include=self.include_fields,
+            exclude=self.exclude_fields
+        )
+        dct['id'] = obj.key.id()
+        for key, value in self.translate_fields.iteritems():
+            dct[value] = dct.pop(key)
+        for key in dct:
+            dct[key] = getattr(self, 'get_' + key, lambda _: dct[key])(obj)
+        return dct
+
+    def update(self, data, id, model=None, partial=False):
+        return self._save(data, instance=self.get_obj(id=id, model=model))
