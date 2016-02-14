6767bf995f61bf5db1ad7bd3f5218a5447182ce4
adesor <abhinav@grofers.com>
2016-02-14 15:35:41 +0530

    Bump version number

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'zennla',
   packages = ['zennla'],
-  version = '0.3',
+  version = '0.4',
   description = 'A REST microframework for Google App Engine',
   author = 'Abhinav Desor',
   author_email = 'abhinav.desor@gmail.com',
   url = 'https://github.com/adesor/zenn-la',
-  download_url = 'https://github.com/adesor/pokeball/tarball/0.3',
+  download_url = 'https://github.com/adesor/pokeball/tarball/0.4',
   keywords = ['google app engine', 'app engine', 'gae', 'ndb', 'webapp2', 'rest', 'framework'],
   classifiers = [],
 )
